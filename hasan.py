6cb6f0a959cdee7d876c9bb6141ce4d28a224561
Hasan <s1149981@student.hsleiden.nl>
2023-12-06 22:46:50 +0100

    a

--- hasan.py
+++ hasan.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
+import RPi.GPIO as GPIO
+import time
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4, GPIO.IN)
 
-#jatoch
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+for i in range(20):
+    print(GPIO.input(4))
+    time.sleep(1)  # Hier kun je aangeven hoe lang je wilt wachten tussen elke meting
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+GPIO.cleanup()
