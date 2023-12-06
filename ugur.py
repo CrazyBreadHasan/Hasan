aa2091480e1ea01a289eb1403022d2a39740e330
Hasan <s1149981@student.hsleiden.nl>
2023-12-06 23:24:28 +0100

    a

--- ugur.py
+++ ugur.py
@@ -10,9 +10,9 @@ been_pressed = 0
 
 
 while True:
-    if GPIO.input(4) == 1 and been_pressed == 1:
+    if GPIO.input(4) == 0 and been_pressed == 0:
         print("Je kan op een knop klikken (wooow)")
-    elif been_pressed == 0 and GPIO.input(4) == 0:
+    elif been_pressed == 1 and GPIO.input(4) == 1:
         print("Been released")
         time.sleep(0.5)
         break
