2ecf0ea9912ba9b55a59cb3114df785183ce2f25
Hasan <s1149981@student.hsleiden.nl>
2023-12-06 23:04:28 +0100

    a

--- hasan.py
+++ hasan.py
@@ -6,6 +6,6 @@ GPIO.setup(4, GPIO.IN)
 
 for i in range(20):
     print(GPIO.input(4))
-    time.sleep(1)  # Hier kun je aangeven hoe lang je wilt wachten tussen elke meting
+    time.sleep(0.5)  # Hier kun je aangeven hoe lang je wilt wachten tussen elke meting
 
 GPIO.cleanup()
