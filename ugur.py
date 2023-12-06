67b0295f3826e570a60ad3166683dd15f36f5084
Hasan <s1149981@student.hsleiden.nl>
2023-12-06 23:16:30 +0100

    a

--- ugur.py
+++ ugur.py
@@ -10,9 +10,9 @@ status = 1
 been_pressed = 0
 
 while status == 1:
-    if GPIO.input(4) == 0 and been_pressed == 0:
+    if GPIO.input(4) == 1 and been_pressed == 1:
         print("Been pressed")
-    elif been_pressed == 1 and GPIO.input(4)  == 1:
+    elif been_pressed == 0 and GPIO.input(4) == 0:
         print("Been released")
         time.sleep(0.5)
         status = 0
