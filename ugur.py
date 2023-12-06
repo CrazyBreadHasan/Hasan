21f80687368babd9abd461fc66e77259d2b8ada2
Hasan <s1149981@student.hsleiden.nl>
2023-12-06 23:11:28 +0100

    a

---
+++ ugur.py
@@ -0,0 +1,19 @@
+import RPi.GPIO as GPIO
+import time
+
+import gpio
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4, GPIO.IN)
+
+status = 1
+been_pressed = 0
+
+while status == 1:
+    if GPIO.input(4) == 0 and been_pressed == 0:
+        print("Been pressed")
+    elif been_pressed == 1 and status == 1:
+        print("Been released")
+        time.sleep(0.5)
+        status = 0
+gpio.cleanup()
