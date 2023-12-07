1fa2e4cc1a4dc78d4a2dd2ed584150c5ca53ee31
Hasan <s1149981@student.hsleiden.nl>
2023-12-07 15:55:49 +0100

    a

--- led1.py
+++ led1.py
@@ -8,13 +8,15 @@ GPIO.setup(17, GPIO.OUT)
 
 for i in range(10):
     GPIO.output(4, GPIO.HIGH)
-    GPIO.output(4, GPIO.LOW)
     time.sleep(0.5)
+    GPIO.output(4, GPIO.LOW)
+
 
 
     GPIO.output(17, GPIO.HIGH)
-    GPIO.output(17, GPIO.LOW)
     time.sleep(0.5)
+    GPIO.output(17, GPIO.LOW)
+
 
 
 GPIO.cleanup()
