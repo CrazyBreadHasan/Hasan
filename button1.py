21f61ef684ebb27455dbd664668946684e2ef22e
Hasan <s1149981@student.hsleiden.nl>
2023-12-07 16:33:01 +0100

    a

---
+++ button1.py
@@ -0,0 +1,20 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(4, GPIO.OUT)
+GPIO.setup(17, GPIO.OUT)
+GPIO.setup(22, GPIO.IN)
+
+for i in range(10):
+    if GPIO.input(22) == True:
+        GPIO.output(4, GPIO.HIGH)
+        GPIO.output(17, GPIO.LOW)
+    elif GPIO.input(22) == False:
+        GPIO.output(4, GPIO.LOW)
+        GPIO.output(17, GPIO.HIGH)
+
+    time.sleep(0.5)
+
+GPIO.cleanup()
