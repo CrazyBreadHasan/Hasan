d40671f98128efec15b50c1af993b123490ca6b9
Hasan <s1149981@student.hsleiden.nl>
2023-12-08 18:04:42 +0100

    a

---
+++ led2.py
@@ -0,0 +1,33 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(4, GPIO.OUT)
+GPIO.setup(17, GPIO.OUT)
+GPIO.setup(16, GPIO.OUT)
+GPIO.setup(21, GPIO.OUT)
+
+for i in range(10):
+
+    GPIO.output(4, GPIO.HIGH)
+    GPIO.output(21, GPIO.HIGH)
+    time.sleep(0.25)
+    GPIO.output(4, GPIO.LOW)
+    GPIO.output(21, GPIO.LOW)
+
+
+
+    GPIO.output(17, GPIO.HIGH)
+    time.sleep(0.25)
+    GPIO.output(17, GPIO.LOW)
+
+
+    GPIO.output(16, GPIO.HIGH)
+    time.sleep(0.25)
+    GPIO.output(16, GPIO.LOW)
+#ok
+
+
+
+GPIO.cleanup()
