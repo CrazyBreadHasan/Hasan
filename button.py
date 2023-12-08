a3d447364cbb8e365c5902ca8c0a4a207faf472e
Hasan <s1149981@student.hsleiden.nl>
2023-12-08 09:33:26 +0100

    a

--- button.py
+++ button.py
@@ -7,13 +7,14 @@ GPIO.setup(4, GPIO.OUT)
 GPIO.setup(17, GPIO.OUT)
 GPIO.setup(22, GPIO.IN)
 
-for i in range(10):
 
-    if GPIO.input(22) == True:
+# for i in range(10):
+
+if GPIO.input(22) == True:
         GPIO.output(4, GPIO.HIGH)
         time.sleep(0.5)
         GPIO.output(4, GPIO.LOW)
-    elif GPIO.input(22) == False:
+elif GPIO.input(22) == False:
         GPIO.output(17, GPIO.HIGH)
         time.sleep(0.5)
         GPIO.output(17, GPIO.LOW)
