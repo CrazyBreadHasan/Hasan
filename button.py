7c9ab3cbf798bf61d7b6d895829c38b2d0b23e02
Hasan <s1149981@student.hsleiden.nl>
2023-12-08 09:55:32 +0100

    a

--- button.py
+++ button.py
@@ -14,11 +14,11 @@ while True:
         if GPIO.input(22) == True:
                 GPIO.output(4, GPIO.HIGH)
                 time.sleep(0.5)
-                GPIO.output(17, GPIO.LOW)
+                GPIO.output(4, GPIO.LOW)
 
         elif GPIO.input(22) == False:
                 GPIO.output(17, GPIO.HIGH)
                 time.sleep(0.5)
-                GPIO.output(4, GPIO.LOW)
+                GPIO.output(17, GPIO.LOW)
 
 GPIO.cleanup()
