3133ea7715e2c845e75e600b81e17c3917a8df88
Hasan <s1149981@student.hsleiden.nl>
2023-12-08 16:55:02 +0100

    a

--- cimbom.py
+++ cimbom.py
@@ -6,14 +6,17 @@ try:
 
     GPIO.setup(4, GPIO.OUT)
     GPIO.setup(17, GPIO.OUT)
-    GPIO.setup(24, GPIO.OUT)
+    GPIO.setup(16, GPIO.OUT)
     GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
     while True:
-        if GPIO.input(22) and GPIO.input(24) == True:
+        if GPIO.input(22) == True:
             GPIO.output(4, GPIO.HIGH)
             time.sleep(0.5)
             GPIO.output(4, GPIO.LOW)
+            GPIO.output(16, GPIO.HIGH)
+            time.sleep(0.5)
+            GPIO.output(16, GPIO.LOW)
 
         elif GPIO.input(22) == False:
             GPIO.output(17, GPIO.HIGH)
