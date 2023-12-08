a2270c5bfbac18e69a9b47e00584d3d070461564
Hasan <s1149981@student.hsleiden.nl>
2023-12-09 00:04:34 +0100

    a

--- buzzer.py
+++ buzzer.py
@@ -1,19 +1,28 @@
+#importeer GPIO en time package
 import RPi.GPIO as GPIO
 import time
-# Use BCM mode for the pin numbering (conform breakout)
+#gebruik BCM mode voor de pin nummering (conform breakout)
 GPIO.setmode(GPIO.BCM)
-# Set the pin as output
+#zet de pin als output
 GPIO.setup(4, GPIO.OUT)
-p= GPIO.PWM(4, 1000)
+# loop through 50 times
+for i in range(50):
+ GPIO.output(4, GPIO.HIGH)
+ time.sleep(0.025)
+ GPIO.output(4, GPIO.LOW)
+ time.sleep(0.025)
+GPIO.cleanup()
 
-def playTone(duration):
-    p.start(50)
-    time.sleep(duration)
-    p.stop()
+def beep(freq, duration):
+#frequency in Hz
+#duration in seconds
+ period = 1.0/freq
 
-try:
-    # Play a tone for half a second
-    playTone(0.5)
-finally:
-    # Clean up the GPIO pins
-    GPIO.cleanup()
+def sweep(start_freq, end_freq, duration):
+# deze functie start met de start frequentie en
+# aan het eind (t + duration) is de frequentie opgelopen
+# (of afgenomen) tot de end frequentie
+ start_period = 1.0/start_freq
+ end_period = 1.0/end_freq
+ avg_period = (start_period + end_period) / 2
+ freq_delta = (end_freq – start_freq)/avg_period
