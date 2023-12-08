d71dd2f777d785096ee463c5a8d508e88a9df44b
Hasan <s1149981@student.hsleiden.nl>
2023-12-08 17:52:04 +0100

    a

--- led1.py
+++ led1.py
@@ -5,11 +5,12 @@ GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(4, GPIO.OUT)
 GPIO.setup(17, GPIO.OUT)
+GPIO.setup(16, GPIO.OUT)
 
 for i in range(10):
 
     GPIO.output(4, GPIO.HIGH)
-    time.sleep(0.5)
+    time.sleep(0.25)
     GPIO.output(4, GPIO.LOW)
 
 
@@ -17,6 +18,10 @@ for i in range(10):
     GPIO.output(17, GPIO.HIGH)
     time.sleep(0.25)
     GPIO.output(17, GPIO.LOW)
+
+    GPIO.output(16, GPIO.HIGH)
+    time.sleep(0.25)
+    GPIO.output(16, GPIO.LOW)
 
 
 
