1f9e519572f8f1171443dd6d0f3e5887205eaf37
Hasan <s1149981@student.hsleiden.nl>
2023-12-10 21:49:45 +0100

    a

--- quizpro.py
+++ quizpro.py
@@ -1,6 +1,53 @@
 import requests
+import html
+import random
 
-url = "https://www.mentimeter.com/blog/stand-out-get-ahead/trivia-questions"
+# vragen ophalen
+def vragen_ophalen(amount: int, category: int) -> list:
+    url = f"https://opentdb.com/api.php?amount={amount}&category={category}"
+    response = requests.get(url)
+    response_json = response.json()
+    return response_json["results"]
 
-response = requests.get(url)
-print(response.text)
+# vragen mixen
+def vragen_mixen(keuze: list) ->list:
+    random.shuffle(keuze)
+    return keuze
+
+
+# antwoorden printen
+def print_keuzes(keuzes: list) -> None:
+    for keuze_index, keuze in enumerate(keuzes):
+        print(f"{keuze_index+1}. {html.unescape(keuze)}")
+
+# antwoord van gebruiker
+def pak_gebruiker_keuze() -> int:
+    while True:
+        gebruiker_keuze = int(input("Voer nummer van je keuze in: "))
+        if gebruiker_keuze in range(1, 5):
+            return gebruiker_keuze - 1
+        else:
+            print("Invalide antwoord, Voer nummer van je keuze in.")
+
+# spel spelen
+def speel_spel(amount: int, catogory: int) -> None:
+    ophalen = vragen_ophalen(amount, catogory)
+    for vraag in ophalen:
+        vraag_text = html.unescape(vraag["question"])
+        print(vraag_text)
+        keuzes = vraag["incorrect_answers"]
+        keuzes.extend([vraag["correct_answer"]])
+        mix_vragen = vragen_mixen(keuzes)
+        print_keuzes(mix_vragen)
+        geb_keuze_index = pak_gebruiker_keuze()
+        geb_keuze_tekst = mix_vragen[geb_keuze_index]
+        juiste_antwoord_tekst = html.unescape(vraag["correct_answer"])
+        if geb_keuze_index == juiste_antwoord_tekst:
+            print("Juist!!!. ")
+        else:
+            print("incorrect")
+
+if __name__ == '__main__':
+    amount = 3
+    catogoty = 18
+    speel_spel(amount, catogoty)
