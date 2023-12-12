62991359ecc1f8416d609ba906828b420eaccd8d
Hasan <s1149981@student.hsleiden.nl>
2023-12-12 19:12:11 +0100

    a

--- test.py
+++ test.py
@@ -7,5 +7,5 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(4, GPIO.OUT)
 GPIO.setup(17, GPIO.OUT)
 
-GPIO.output(4, GPIO.HIGH)
-GPIO.output(17, GPIO.HIGH)
+GPIO.output(4 and 17, GPIO.HIGH)
+# GPIO.output(17, GPIO.HIGH)
