d23a5e1d1b17ef308f34ba30a83f7b9d049677d0
Hasan <s1149981@student.hsleiden.nl>
2023-12-12 20:35:35 +0100

    a

--- test.py
+++ test.py
@@ -3,11 +3,11 @@ import time
 
 # Set the mode and configure the GPIO pin
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(17, GPIO.OUT)
+GPIO.setup(4, GPIO.OUT)
 
 # Turn on the light
 
 for i in range(5):
-    GPIO.output(17, GPIO.HIGH)
+    GPIO.output(4, GPIO.HIGH)
     time.sleep(0.25)
-    GPIO.output(17, GPIO.LOW)
+    GPIO.output(4, GPIO.LOW)
