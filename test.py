f5656296dee44d71241c58e06fa2020f80e4eaa2
Hasan <s1149981@student.hsleiden.nl>
2023-12-12 19:14:29 +0100

    a

--- test.py
+++ test.py
@@ -1,11 +1,14 @@
-#importeer GPIO en time package
 import RPi.GPIO as GPIO
 import time
-#gebruik BCM mode voor de pin nummering (conform breakout)
+
 GPIO.setmode(GPIO.BCM)
-#zet de pin als output
 GPIO.setup(4, GPIO.OUT)
 GPIO.setup(17, GPIO.OUT)
 
-GPIO.output(4 and 17, GPIO.HIGH)
-# GPIO.output(17, GPIO.HIGH)
+GPIO.output(4, GPIO.HIGH)
+GPIO.output(17, GPIO.HIGH)
+
+# Add a delay or other code here if needed
+
+# Cleanup GPIO settings when done
+GPIO.cleanup())
