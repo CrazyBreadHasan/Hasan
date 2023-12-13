be2eb0a56b23d8b9cf92e14dafd305252f343c1f
Hasan <s1149981@student.hsleiden.nl>
2023-12-13 22:44:12 +0100

    a

--- scroll1.py
+++ scroll1.py
@@ -1,40 +1,25 @@
-import RPi.GPIO as GPIO
 import drivers
 from time import sleep
 
-# Load the driver and set it to "display"
-display = drivers.Lcd()
+display = drivers.lcd()
 
-# Set the GPIO mode to BCM
-GPIO.setmode(GPIO.BCM)
 
-# Set the pin number for the backlight
-backlight_pin = 18
+def long_string(display, text='', num_line=1, num_cols=16):
+    """
+    Parameters: (driver, string to print, number of line to print, number of columns of your display)
+    Return: This function send to display your scrolling string.
+    """
+    if len(text) > num_cols:
+        display.lcd_display_string(text[:num_cols], num_line)
+        sleep(1)
+        for i in range(len(text) - num_cols + 1):
+            text_to_print = text[i:i + num_cols]
+            display.lcd_display_string(text_to_print, num_line)
+            sleep(0.2)
+        sleep(1)
+    else:
+        display.lcd_display_string(text, num_line)
 
-# Set the pin as output
-GPIO.setup(backlight_pin, GPIO.OUT)
 
-# Create a PWM object with a frequency of 100 Hz
-pwm = GPIO.PWM(backlight_pin, 100)
-
-# Start the PWM with a duty cycle of 0 (off)
-pwm.start(0)
-
-# Set the backlight color to green
-display.backlight(display.GREEN)
-
-# Loop through different duty cycles to change the brightness
-try:
-    while True:
-        # Increase the brightness from 0 to 100
-        for dc in range(0, 101, 5):
-            pwm.ChangeDutyCycle(dc)
-            sleep(0.1)
-        # Decrease the brightness from 100 to 0
-        for dc in range(100, -1, -5):
-            pwm.ChangeDutyCycle(dc)
-            sleep(0.1)
-except KeyboardInterrupt:
-    # Stop the PWM and clean up the GPIO
-    pwm.stop()
-    GPIO.cleanup()
+long_string(display, "Hello World!", 1)
+sleep(1)
--- time.py
+++ time.py
@@ -25,3 +25,4 @@ except KeyboardInterrupt:
     # If there is a KeyboardInterrupt (when you press ctrl+c), exit the program and cleanup
     print("Cleaning up!")
     display.lcd_clear()
+
