dceeddf00c418031c9c050569181f98b74ac8355
Hasan <s1149981@student.hsleiden.nl>
2023-12-13 23:10:37 +0100

    a

--- proquiz.py
+++ proquiz.py
@@ -104,14 +104,14 @@ def speel_spel(amount: int, catogory: int) -> None:
         geb_keuze_tekst = mix_vragen[geb_keuze_index]
         juiste_antwoord_tekst = html.unescape(vraag["correct_answer"])
         if geb_keuze_tekst == juiste_antwoord_tekst:
-            print("Juist!!!. ")
+            long_string(display, "Juist", 2)
             led_aan_groen()
             punten +=1
 
 
 
         elif geb_keuze_tekst != juiste_antwoord_tekst:
-            print("incorrect")
+            long_string(display, "Incorrect", 2)
             led_aan_rood()
             punten -= 1
 
