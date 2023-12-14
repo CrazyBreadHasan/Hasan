4791794e659206492e4defcd16f2078180a37092
Hasan <s1149981@student.hsleiden.nl>
2023-12-14 18:29:06 +0100

    a

--- rotary.py
+++ rotary.py
@@ -11,29 +11,33 @@ GPIO.setup(dt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 counter = 0  # Start at 0
 clkLastState = GPIO.input(clk)
 
+# Define a callback function that updates the counter
+def update_counter(channel):
+    global counter, clkLastState
+    clkState = GPIO.input(clk)
+    dtState = GPIO.input(dt)
+    if clkState != clkLastState:
+        if dtState != clkState:
+            counter += 1
+        else:
+            counter -= 1
+        # Ensure the counter stays within the range [0, 20]
+        counter = max(0, min(20, counter))
+        # print(counter)
+        print(dtState)
+        # If the counter has reached 20, exit the program
+        if counter == 20:
+            GPIO.cleanup()
+            exit()
+    clkLastState = clkState
+
+# Add event detection to the clk and dt pins
+GPIO.add_event_detect(clk, GPIO.BOTH, callback=update_counter)
+GPIO.add_event_detect(dt, GPIO.BOTH, callback=update_counter)
+
+# Wait for the user to press Ctrl+C
 try:
     while True:
-        clkState = GPIO.input(clk)
-        dtState = GPIO.input(dt)
-
-        if clkState != clkLastState:
-            if dtState != clkState:
-                counter += 1
-            else:
-                counter -= 1
-
-            # Ensure the counter stays within the range [0, 20]
-            counter = max(0, min(20, counter))
-
-            # print(counter)
-            print(dtState)
-
-            # If the counter has reached 20, exit the loop
-            if counter == 20:
-                break
-
-        clkLastState = clkState
-        sleep(0.01)
-
-finally:
+        sleep(1)
+except KeyboardInterrupt:
     GPIO.cleanup()
