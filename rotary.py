979bbe64f5ffd1904886f157bc1d62e54e7e22ac
Hasan <s1149981@student.hsleiden.nl>
2023-12-14 19:33:10 +0100

    a

--- rotary.py
+++ rotary.py
@@ -16,22 +16,36 @@ counter = 0
 clklaststate = GPIO.input(clk)
 dtlaststate = GPIO.input(dt)
 
+# add a variable to store the last interrupt time
+last_interrupt_time = 0
+
+# add a constant to define the minimum time between interrupts
+DEBOUNCE_TIME = 0.01  # in seconds
+
 try:
     while True:
         clkstate = GPIO.input(clk)
         dtstate = GPIO.input(dt)
         if clkstate != clklaststate or dtstate != dtlaststate:  # check both pins for changes
-            if clkstate != dtlaststate:  # if they are different, it means clockwise rotation
-                counter += 1
-            else:  # if they are the same, it means counter-clockwise rotation
-                counter -= 1
-            print(counter)
+            # get the current time
+            current_time = time.time()
+            # check if enough time has passed since the last interrupt
+            if current_time - last_interrupt_time > DEBOUNCE_TIME:
+                # update the counter
+                if clkstate != dtlaststate:  # if they are different, it means clockwise rotation
+                    counter += 1
+                else:  # if they are the same, it means counter-clockwise rotation
+                    counter -= 1
+                print(counter)
 
-            # add the if statement to limit the counter range
-            if counter < 0:  # if the counter is below the minimum
-                counter = 0  # reset it to 0
-            elif counter > 20:  # if the counter is above the maximum
-                counter = 20  # reset it to 20
+                # add the if statement to limit the counter range
+                if counter < 0:  # if the counter is below the minimum
+                    counter = 0  # reset it to 0
+                elif counter > 20:  # if the counter is above the maximum
+                    counter = 20  # reset it to 20
+
+            # update the last interrupt time
+            last_interrupt_time = current_time
 
         clklaststate = clkstate  # update the previous states
         dtlaststate = dtstate
