68ed09fdaf986238285fbfa15172ef49d8d38cd8
Hasan <s1149981@student.hsleiden.nl>
2023-12-14 19:44:44 +0100

    a

--- rotary1.py
+++ rotary1.py
@@ -1,55 +1,28 @@
 import RPi.GPIO as GPIO
-import time
-
-from RPi import GPIO
 from time import sleep
 
-clk = 17
-dt = 18
+counter = 0 # define a counter variable
+Enc_A = 17 # define the GPIO pin for the CLK pin of the encoder
+Enc_B = 18 # define the GPIO pin for the DT pin of the encoder
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(clk, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(dt, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setmode(GPIO.BCM) # set the GPIO mode to BCM
+GPIO.setup(Enc_A, GPIO.IN) # set the CLK pin as input
+GPIO.setup(Enc_B, GPIO.IN) # set the DT pin as input
 
-counter = 0
-counter = max(0, min(20, counter)) # clip the counter to 0 to 20
-
-clklaststate = GPIO.input(clk)
-dtlaststate = GPIO.input(dt)
-
-# add a variable to store the last interrupt time
-last_interrupt_time = 0
-
-# add a constant to define the minimum time between interrupts
-DEBOUNCE_TIME = 0.01  # in seconds
+clklaststate = GPIO.input(Enc_A) # get the initial state of the CLK pin
 
 try:
-    while True:
-        clkstate = GPIO.input(clk)
-        dtstate = GPIO.input(dt)
-        if clkstate != clklaststate or dtstate != dtlaststate:  # check both pins for changes
-            # get the current time
-            current_time = time.time()
-            # check if enough time has passed since the last interrupt
-            if current_time - last_interrupt_time > DEBOUNCE_TIME:
-                # update the counter
-                if clkstate == 0 and dtstate == 1:  # if the pins are in this state, it means clockwise rotation
-                    counter += 1
-                # else: # ignore the other states, as they mean counter-clockwise rotation
-                #     pass
-                print(counter)
+    while True: # loop forever
+        clkstate = GPIO.input(Enc_A) # get the current state of the CLK pin
+        dtstate = GPIO.input(Enc_B) # get the current state of the DT pin
+        if clkstate != clklaststate: # if the CLK pin state has changed, the encoder is being rotated
+            if dtstate != clkstate: # if the DT pin state is different from the CLK pin state, it means clockwise rotation
+                counter += 1 # increment the counter
+            else: # if the DT pin state is the same as the CLK pin state, it means counter-clockwise rotation
+                counter -= 1 # decrement the counter
+            print(counter) # print the counter value
 
-                # add the if statement to limit the counter range
-                if counter < 0:  # if the counter is below the minimum
-                    counter = 0  # reset it to 0
-                elif counter > 20:  # if the counter is above the maximum
-                    counter = 20  # reset it to 20
-
-            # update the last interrupt time
-            last_interrupt_time = current_time
-
-        clklaststate = clkstate  # update the previous states
-        dtlaststate = dtstate
-        sleep(0.001)
-finally:
-    GPIO.cleanup()
+        clklaststate = clkstate # update the previous state of the CLK pin
+        sleep(0.001) # add a small delay to avoid bouncing
+except KeyboardInterrupt: # if the user presses Ctrl+C, exit the program
+    GPIO.cleanup() # clean up the GPIO pins
