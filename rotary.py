5b4a000f98541fad1e033979d28298f29d1accc6
Hasan <s1149981@student.hsleiden.nl>
2023-12-14 18:49:28 +0100

    a

--- rotary.py
+++ rotary.py
@@ -1,43 +1,32 @@
 import RPi.GPIO as GPIO
+import time
+
+from RPi import GPIO
 from time import sleep
 
 clk = 17
 dt = 18
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(clk, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(dt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(clk, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(dt, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-counter = 0  # Start at 0
-clkLastState = GPIO.input(clk)
+counter = 0
 
-# Define a callback function that updates the counter
-def update_counter(channel):
-    global counter, clkLastState
-    clkState = GPIO.input(clk)
-    dtState = GPIO.input(dt)
-    if clkState != clkLastState:
-        if dtState != clkState:
-            counter += 1
-        else:
-            counter -= 1
-        # Ensure the counter stays within the range [0, 20]
-        counter = max(0, min(20, counter))
-        # print(counter)
-        print(counter)
-        # If the counter has reached 20, exit the program
-        if counter == 20:
-            GPIO.cleanup()
-            exit()
-    clkLastState = clkState
+clklaststate = GPIO.input(clk)
 
-# Add event detection to the clk and dt pins
-GPIO.add_event_detect(clk, GPIO.BOTH, callback=update_counter)
-GPIO.add_event_detect(dt, GPIO.BOTH, callback=update_counter)
-
-# Wait for the user to press Ctrl+C
 try:
     while True:
-        sleep(1)
-except KeyboardInterrupt:
+        clkstate = GPIO.input(clk)
+        dtstate = GPIO.input(dt)
+        if clkstate != clklaststate:
+            if dtstate != clklaststate:
+                counter += 1
+            else:
+                counter -= 1
+            print(counter)
+
+        clklaststate = clkstate
+        sleep(0.001)
+finally:
     GPIO.cleanup()
