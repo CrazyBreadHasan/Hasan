c7768e1cb4d6c0e379fdb9c14250dd6128833085
Hasan <s1149981@student.hsleiden.nl>
2023-12-15 16:55:40 +0100

    a

--- roatary.py
+++ roatary.py
@@ -1,88 +1,92 @@
 import RPi.GPIO as GPIO
-class RotaryEncoder:
- CLOCKWISE=1
- ANTICLOCKWISE=2
- BUTTONDOWN=3
- BUTTONUP=4
- rotary_a = 0
- rotary_b = 0
- rotary_c = 0
- last_state = 0
- direction = 0
 
- # Initialise rotary encoder object
- def __init__(self, pinA, pinB, button, callback):
+
+class RotaryEncoder:
+    CLOCKWISE = 1
+    ANTICLOCKWISE = 2
+    BUTTONDOWN = 3
+    BUTTONUP = 4
+
+    rotary_a = 0
+    rotary_b = 0
+    rotary_c = 0
+    last_state = 0
+    direction = 0
+
+    # Initialise rotary encoder object
+    def __init__(self, pinA, pinB, button, callback):
         self.pinA = pinA
         self.pinB = pinB
         self.button = button
         self.callback = callback
         GPIO.setmode(GPIO.BCM)
-
         GPIO.setwarnings(False)
         GPIO.setup(self.pinA, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         GPIO.setup(self.pinB, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         GPIO.setup(self.button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        # For version 1 (old) boards comment out the above four lines
- # and un-comment the following 3 lines
-        GPIO.setup(self.pinA, GPIO.IN)
-        GPIO.setup(self.pinB, GPIO.IN)
-        GPIO.setup(self.button, GPIO.IN)
- # Add event detection to the GPIO inputs
+
+        # Add event detection to the GPIO inputs
         GPIO.add_event_detect(self.pinA, GPIO.BOTH, callback=self.switch_event)
         GPIO.add_event_detect(self.pinB, GPIO.BOTH, callback=self.switch_event)
         GPIO.add_event_detect(self.button, GPIO.BOTH, callback=self.button_event)
-        return
 
-        # Call back routine called by switch events
-
-def switch_event(self, switch):
-    if GPIO.input(self.pinA):
-        self.rotary_a = 1
-    else:
-        self.rotary_a = 0
-
-    if GPIO.input(self.pinB):
-        self.rotary_b = 1
-    else:
-        self.rotary_b = 0
-
-    # print(str(self.rotary_a) + str(self.rotary_b))
-    self.rotary_c = self.rotary_a ^ self.rotary_b
-    new_state = self.rotary_a * 4 + self.rotary_b * 2 + self.rotary_c * 1
-    delta = (new_state - self.last_state) % 4
-    self.last_state = new_state
-    event = 0
-
-    if delta == 1:
-        if self.direction == self.CLOCKWISE:
-            # print "Clockwise"
-            event = self.direction
+    # Call back routine called by switch events
+    def switch_event(self, switch):
+        if GPIO.input(self.pinA):
+            self.rotary_a = 1
         else:
-            self.direction = self.CLOCKWISE
-    elif delta == 3:
-        if self.direction == self.ANTICLOCKWISE:
-            # print "Anticlockwise"
-            event = self.direction
+            self.rotary_a = 0
+
+        if GPIO.input(self.pinB):
+            self.rotary_b = 1
         else:
-            self.direction = self.ANTICLOCKWISE
+            self.rotary_b = 0
 
-    if event > 0:
-        self.callback(event)
+        self.rotary_c = self.rotary_a ^ self.rotary_b
+        new_state = self.rotary_a * 4 + self.rotary_b * 2 + self.rotary_c * 1
+        delta = (new_state - self.last_state) % 4
+        self.last_state = new_state
+        event = 0
 
-    return
+        if delta == 1:
+            if self.direction == self.CLOCKWISE:
+                event = self.direction
+            else:
+                self.direction = self.CLOCKWISE
+        elif delta == 3:
+            if self.direction == self.ANTICLOCKWISE:
+                event = self.direction
+            else:
+                self.direction = self.ANTICLOCKWISE
+
+        if event > 0:
+            self.callback(event)
+
+    def button_event(self, button):
+        if GPIO.input(button):
+            event = self.BUTTONUP
+        else:
+            event = self.BUTTONDOWN
+            self.callback(event)
+
+    def getSwitchState(self, switch):
+        return GPIO.input(switch)
 
 
-def button_event(self,button):
- if GPIO.input(button):
-    event = self.BUTTONUP
- else:
-    event = self.BUTTONDOWN
-    self.callback(event)
-    return
-
-def getSwitchState(self, switch):
-    return GPIO.input(switch)
+# Example usage:
+PIN_A = 14
+PIN_B = 15
+BUTTON = 18
 
 
+def callback_function(event):
+    print(f"Event: {event}")
 
 
+rswitch = RotaryEncoder(PIN_A, PIN_B, BUTTON, callback_function)
+
+try:
+    while True:
+        pass
+except KeyboardInterrupt:
+    GPIO.cleanup()
