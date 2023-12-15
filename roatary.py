d42fe943d2f60d82afdb8b2f21c12ece508c4534
Hasan <s1149981@student.hsleiden.nl>
2023-12-15 12:55:36 +0100

    a

--- roatary.py
+++ roatary.py
@@ -82,3 +82,31 @@ def button_event(self,button):
 
 def getSwitchState(self, switch):
  return GPIO.input(switch)
+
+
+import sys
+import time
+from rotary_class import RotaryEncoder
+
+# Define GPIO inputs
+PIN_A = 17 # Pin 8
+PIN_B = 18 # Pin 10
+BUTTON = 4 # Pin 7
+
+def switch_event(event):
+    if event == RotaryEncoder.CLOCKWISE:
+        print("Clockwise")
+    elif event == RotaryEncoder.ANTICLOCKWISE:
+        print("Anticlockwise")
+    elif event == RotaryEncoder.BUTTONDOWN:
+        print("Button down")
+    elif event == RotaryEncoder.BUTTONUP:
+        print("Button up")
+    return
+
+rswitch = RotaryEncoder(PIN_A,PIN_B,BUTTON,switch_event)
+
+while True:
+ for counter in range(120):
+    print(counter)
+    time.sleep(2.0)
