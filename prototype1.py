aa12dabd521b978f39205ca1de89155d9b6bb9de
Hasan <s1149981@student.hsleiden.nl>
2023-12-15 17:51:32 +0100

    a

--- prototype1.py
+++ prototype1.py
@@ -26,19 +26,19 @@ fout = 0
 
 def switch_event(event):
     counter = 0
-    while True:
-        if event == RotaryEncoder.CLOCKWISE:
-            counter +=1
-        elif event == RotaryEncoder.ANTICLOCKWISE:
-            counter -=1
 
-        elif event == RotaryEncoder.BUTTONDOWN:
+    if event == RotaryEncoder.CLOCKWISE:
+            counter += 1
+    elif event == RotaryEncoder.ANTICLOCKWISE:
+            counter -= 1
+
+    elif event == RotaryEncoder.BUTTONDOWN:
             print("Button down")
-        elif event == RotaryEncoder.BUTTONUP:
+    elif event == RotaryEncoder.BUTTONUP:
             print("Button up")
-        counter = max(0, min(20, counter))
+    counter = max(0, min(20, counter))
 
-        print(counter)
+    print(counter)
 
 
 def encoder():
