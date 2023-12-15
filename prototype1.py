4981ed5af1ba86a39bd2cbc12ea12d29f00006e0
Hasan <s1149981@student.hsleiden.nl>
2023-12-15 11:51:24 +0100

    a

--- prototype1.py
+++ prototype1.py
@@ -53,7 +53,7 @@ def encoder():
                 counter = max(0, min(20, counter))
                 print(counter)
 
-                if 0 <= counter <= 5:
+                if counter in range(0, 6):
                     if GPIO.input(clk) == True:
                         # and (GPIO.input(clk, GPIO.LOW))):
                         print("Hello")
---
+++ roatary.py
@@ -0,0 +1,25 @@
+import sys
+import time
+from rotary_class import RotaryEncoder
+# Define GPIO inputs
+PIN_A = 17 # Pin 8
+PIN_B = 18 # Pin 10
+BUTTON = 4 # Pin 7
+# This is the event callback routine to handle events
+def switch_event(event):
+ if event == RotaryEncoder.CLOCKWISE:
+ print("Clockwise")
+ elif event == RotaryEncoder.ANTICLOCKWISE:
+ print("Anticlockwise")
+ elif event == RotaryEncoder.BUTTONDOWN:
+ print("Button down")
+ elif event == RotaryEncoder.BUTTONUP:
+ print("Button up")
+ return
+# Define the switch
+rswitch = RotaryEncoder(PIN_A,PIN_B,BUTTON,switch_event)
+while True:
+ for counter in range(120):
+     print(counter)
+     time.sleep(2.0)
+
