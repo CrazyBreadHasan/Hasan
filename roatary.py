b62f25d7584fe03205c0fd8df72098e876bf6396
Hasan <s1149981@student.hsleiden.nl>
2023-12-15 11:53:22 +0100

    a

--- roatary.py
+++ roatary.py
@@ -7,15 +7,15 @@ PIN_B = 18 # Pin 10
 BUTTON = 4 # Pin 7
 # This is the event callback routine to handle events
 def switch_event(event):
- if event == RotaryEncoder.CLOCKWISE:
- print("Clockwise")
- elif event == RotaryEncoder.ANTICLOCKWISE:
- print("Anticlockwise")
- elif event == RotaryEncoder.BUTTONDOWN:
- print("Button down")
- elif event == RotaryEncoder.BUTTONUP:
- print("Button up")
- return
+    if event == RotaryEncoder.CLOCKWISE:
+        print("Clockwise")
+    elif event == RotaryEncoder.ANTICLOCKWISE:
+        print("Anticlockwise")
+    elif event == RotaryEncoder.BUTTONDOWN:
+        print("Button down")
+    elif event == RotaryEncoder.BUTTONUP:
+        print("Button up")
+    return
 # Define the switch
 rswitch = RotaryEncoder(PIN_A,PIN_B,BUTTON,switch_event)
 while True:
