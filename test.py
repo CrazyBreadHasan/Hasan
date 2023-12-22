fb66fd80856812020d5190376a6954012e56c832
Hasan <s1149981@student.hsleiden.nl>
2023-12-22 12:36:52 +0100

    a

--- test.py
+++ test.py
@@ -42,11 +42,13 @@ def print_keuzes(keuzes: list, type_vraag) -> str:
     for keuze_index, keuze in enumerate(keuzes):
         choices_string += f"{keuze_index+1}. {html.unescape(keuze)}\n"
         # splitted_choices = choices_string.split(",")
-        if(type_vraag.__eq__("multiple")):
-            keuze1 = choices_string.split(",")[0]
-            # keuze1, keuze2, keuze3, keuze4 = splitted_choices
-            print(keuze1)
+
         # print(splitted_choices[1])
+
+    if (type_vraag.__eq__("multiple")):
+        keuze1 = choices_string.split(",")[0]
+        # keuze1, keuze2, keuze3, keuze4 = splitted_choices
+        print(keuze1)
 
     return choices_string
 
