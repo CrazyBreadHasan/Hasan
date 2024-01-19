fac206639aa4f29be54b2b92488e58fbe1e6fb98
Hasan <s1149981@student.hsleiden.nl>
2024-01-19 10:01:20 +0100

    a

--- roatary.py
+++ roatary.py
@@ -71,12 +71,13 @@ class RotaryEncoder:
         if event > 0:
             self.callback(event, type_vraag)
 
-    def button_event(self, channel):
+    def button_event(self, channel,type_vraag):
+        print(type_vraag)
         if GPIO.input(channel):
             event = self.BUTTONUP
         else:
             event = self.BUTTONDOWN
-            self.callback(event)
+            self.callback(event, type_vraag)
 
 
     def getSwitchState(self, switch):
