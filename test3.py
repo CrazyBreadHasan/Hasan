1fc8f1112a1a2b13629d14cb8f3e94de54601c6b
Hasan <s1149981@student.hsleiden.nl>
2024-01-22 14:27:37 +0100

    a

--- test3.py
+++ test3.py
@@ -209,7 +209,7 @@ def long_string(display, text='', num_line=1, num_cols=16):
     if len(text) > num_cols:
 
         display.lcd_display_string(text[:num_cols], num_line)
-        sleep(0.5)
+        sleep(0.3)
         for i in range(len(text) - num_cols + 1):
             text_to_print = text[i:i + num_cols]
             display.lcd_display_string(text_to_print, num_line)
