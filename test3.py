8fb9c9846b3b98f6234eddbb59cac71261ef831f
Hasan <s1149981@student.hsleiden.nl>
2024-01-23 20:54:15 +0100

    a

--- test3.py
+++ test3.py
@@ -282,7 +282,7 @@ if __name__ == '__main__':
         dt = 18
         encoder_instance = RotaryEncoder(17, 18, 4, switch_event)
 
-        speel_spel(amount, category, encoder_instance, type_vraag="multiple" or "boolean")
+        speel_spel(amount, category, encoder_instance, type_vraag="multiple")
         print(punten)
     except KeyboardInterrupt:
         print("Keyboard interrupt. Cleaning up GPIO.")
