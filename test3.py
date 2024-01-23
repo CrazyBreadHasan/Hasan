f13722f47e462cfdf118eb90e920279afa299a6c
Hasan <s1149981@student.hsleiden.nl>
2024-01-23 21:07:53 +0100

    a

--- test3.py
+++ test3.py
@@ -84,8 +84,7 @@ def pak_gebruiker_keuze_rotary(encoder_instance: RotaryEncoder, event, type_vraa
                 return 2  # Index for keuze3
             elif counter in range(16, 20):
                 return 3  # Index for keuze4
-            else:
-                return 0
+
 
     # Return a default index (0) if event is not BUTTONDOWN
     time.sleep(10)
