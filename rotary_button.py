20fd1ceae8715ea7dc3243bc3391218268c71571
Hasan <s1149981@student.hsleiden.nl>
2024-01-24 14:29:36 +0100

    a

---
+++ rotary_button.py
@@ -0,0 +1,13 @@
+import os
+import requests
+import html
+import random
+import RPi.GPIO as GPIO
+import time
+from time import sleep
+import drivers
+from os import system
+from roatary import RotaryEncoder
+def pak_gebruiker_keuze(event):
+    if event == RotaryEncoder.BUTTONDOWN:
+        print("Button")
