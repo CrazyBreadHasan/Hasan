332f24d5ff8d684657884e0609b89f3d9deabc9f
Hasan <s1149981@student.hsleiden.nl>
2024-01-24 22:13:31 +0100

    a

--- led.py
+++ led.py
@@ -8,12 +8,14 @@ GPIO.setup(21, GPIO.OUT)
 GPIO.setup(20, GPIO.OUT)
 GPIO.setup(16, GPIO.OUT)
 GPIO.setup(26, GPIO.OUT)
+GPIO.setup(19, GPIO.OUT)
 # loop through 5 times
 for i in range(5):
     GPIO.output(21, GPIO.HIGH)
     GPIO.output(20, GPIO.HIGH)
     GPIO.output(16, GPIO.HIGH)
     GPIO.output(26, GPIO.HIGH)
+    GPIO.output(19, GPIO.HIGH)
     print("Led is on!")
     time.sleep(0.25)
 
@@ -21,6 +23,7 @@ for i in range(5):
     GPIO.output(20, GPIO.LOW)
     GPIO.output(16, GPIO.LOW)
     GPIO.output(26, GPIO.LOW)
+    GPIO.output(19, GPIO.LOW)
     print("Led if off")
     time.sleep(0.25)
 
