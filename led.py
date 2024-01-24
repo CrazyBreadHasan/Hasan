ded21aef1c467af4f9efdf46e997f2e1c28c6180
Hasan <s1149981@student.hsleiden.nl>
2024-01-24 21:58:12 +0100

    a

--- led.py
+++ led.py
@@ -7,17 +7,20 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(21, GPIO.OUT)
 GPIO.setup(20, GPIO.OUT)
 GPIO.setup(16, GPIO.OUT)
+GPIO.setup(26, GPIO.OUT)
 # loop through 5 times
 for i in range(5):
     GPIO.output(21, GPIO.HIGH)
     GPIO.output(20, GPIO.HIGH)
     GPIO.output(16, GPIO.HIGH)
+    GPIO.output(26, GPIO.HIGH)
     print("Led is on!")
     time.sleep(0.25)
 
     GPIO.output(21, GPIO.LOW)
     GPIO.output(20, GPIO.LOW)
     GPIO.output(16, GPIO.LOW)
+    GPIO.output(26, GPIO.LOW)
     print("Led if off")
     time.sleep(0.25)
 
