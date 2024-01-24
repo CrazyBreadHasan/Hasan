a5db7f7553756dc9c5b05f10dcc7dbf8f83a9f05
Hasan <s1149981@student.hsleiden.nl>
2024-01-24 14:16:26 +0100

    a

--- test3.py
+++ test3.py
@@ -77,7 +77,7 @@ def pak_gebruiker_keuze_rotary(encoder_instance: RotaryEncoder, event, type_vraa
     button_pressed = False
     print("Please fill in your answer: ")
     while button_pressed == False:
-        print(event)
+        print(encoder_instance.BUTTONDOWN)
         if event == RotaryEncoder.BUTTONDOWN:
             button_pressed = True
             print("Button pressed")
