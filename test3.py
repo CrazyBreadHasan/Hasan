56725e6de191f7de589b05124cf3ec53f6debb8d
Hasan <s1149981@student.hsleiden.nl>
2024-01-24 14:55:35 +0100

    a

--- test3.py
+++ test3.py
@@ -81,17 +81,17 @@ def pak_gebruiker_keuze_rotary(encoder_instance: RotaryEncoder, event, type_vraa
         # print(encoder_instance.getSwitchState(clk))
         if GPIO.input(4) == GPIO.HIGH:
             button_pressed = True
-            print("Button pressed")
+            print(GPIO.input(4))
             # if type_vraag == "multiple":
-            if counter in range(0, 5):
-                return 0  # Index for keuze1
-            elif counter in range(6, 10):
-                print("Answer 2")
-                return 1  # Index for keuze2
-            elif counter in range(11, 15):
-                return 2  # Index for keuze3
-            elif counter in range(16, 20):
-                return 3  # Index for keuze4
+            # if counter in range(0, 5):
+            #     return 0  # Index for keuze1
+            # elif counter in range(6, 10):
+            #     print("Answer 2")
+            #     return 1  # Index for keuze2
+            # elif counter in range(11, 15):
+            #     return 2  # Index for keuze3
+            # elif counter in range(16, 20):
+            #     return 3  # Index for keuze4
 
     print("Too late")
     # Return a default index (0) if event is not BUTTONDOWN
